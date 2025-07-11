95780e7e87edebb5576740d6494e1bff5eddc740
adamk20 <92745982+adamkennedy123@users.noreply.github.com>
2025-07-12 02:37:34 +0700

    Update bot-whatsapp.js

--- bot-whatsapp.js
+++ bot-whatsapp.js
@@ -17,19 +17,9 @@ client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process',
-            '--disable-gpu',
-        ]
+        args: ['--no-sandbox'],
     }
 });
-
 
 client.on('qr', async (qr) => {
     console.log('Scan QR dengan WhatsApp');
